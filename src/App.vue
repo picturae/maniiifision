042a65564d8f970d76515622f9a512fe0e7c0ba0
TessavWalstijn <tvanwalstijn@picturae.com>
2023-09-07 12:54:53 +0200

    Added linting

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,17 @@ import { HelloWorld } from './components/HelloWorld'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+  </main>
 </template>
 
 <style scoped>
