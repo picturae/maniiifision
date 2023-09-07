dc1340bf328645029f6ffec3c9592412ca9f078b
TessavWalstijn <tvanwalstijn@picturae.com>
2023-09-07 16:09:15 +0200

    Adding GitHub Pages Workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const publicPath = env.NODE_ENV === 'production' ? '/maniiifision/' : ''
+
+  let base = '/'
+  if (publicPath) {
+    base = publicPath
+  }
+
+  return {
+    assetsDir: './assets',
+    publicPath,
+    base,
+    plugins: [vue()],
+  }
 })
