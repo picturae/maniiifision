41603900e45f8eeea974c77e21d7b90405d8f0ce
TessavWalstijn <email@tessavwalstijn.com>
2023-11-03 22:55:46 +0100

    Fixed getting manifest object

--- src/components/GetManifest/GetManifest.vue
+++ src/components/GetManifest/GetManifest.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
 import { Container } from '../Container/'
 import { useManifest } from '../../stores/manifest.store'
+import axios from 'axios'
+import { Manifest } from '@iiif/presentation-3'
 
 const manifestStore = useManifest()
 
-const updateIiif = (event: Event) => {
+const updateIiif = async (event: Event) => {
   const value = (event.target as any).value
   if (event) {
     console.log('comp updated: ', value)
     if (value) {
       manifestStore.setIiifUrl(value)
+      const manifest = await getManifest(value)
+      console.log(manifest)
+      manifestStore.setManifest(manifest)
     }
   }
+}
+
+const getManifest = async (url: string) => {
+  const manifest = async () => {
+    axios
+      .get(url)
+      .then(function (response: any) {
+        console.log(response.data)
+        return response.data as Manifest
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+  return manifest()
 }
 </script>
 
