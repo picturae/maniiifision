cf2aed3e897c2a7057ec1b6e6e2c95736f38f441
Raul Ordonez Garcia <rordonezgarcia@picturae.com>
2023-11-04 08:48:06 +0100

    ESLint fixes on GetManifest.vue

--- src/components/GetManifest/GetManifest.vue
+++ src/components/GetManifest/GetManifest.vue
@@ -20,7 +20,7 @@ const updateIiif = async (event: Event) => {
   if (event) {
     console.log('comp updated: ', value)
     if (value) {
-      loadIIIF(value)
+      await loadIIIF(value)
     }
   }
   manifestStore.updateItems([])
@@ -37,22 +37,26 @@ const getManifest = async (url: string) => {
     })
 }
 
-const defaultUrl = 'https://athenaeumcollecties.nl/collecties/gedigitaliseerde-collecties/manifest/0b266318-3487-11e6-b89c-23313efd728e'
+const defaultUrl =
+  'https://athenaeumcollecties.nl/collecties/gedigitaliseerde-collecties/manifest/0b266318-3487-11e6-b89c-23313efd728e'
 
 onMounted(() => {
   loadIIIF(defaultUrl)
 })
-
 </script>
 
 <template>
-  Example manifests:<br/>
-  https://nijdam.nu/maniiifision-api/manifests/manifest.php<br/>
-  https://athenaeumcollecties.nl/collecties/gedigitaliseerde-collecties/manifest/0b266318-3487-11e6-b89c-23313efd728e<br/>
-  https://iiif.io/api/cookbook/recipe/0021-tagging/manifest.json<br/>
+  Example manifests:<br />
+  https://nijdam.nu/maniiifision-api/manifests/manifest.php<br />
+  https://athenaeumcollecties.nl/collecties/gedigitaliseerde-collecties/manifest/0b266318-3487-11e6-b89c-23313efd728e<br />
+  https://iiif.io/api/cookbook/recipe/0021-tagging/manifest.json<br />
   <Container>
     <!-- type url had native URL checking -->
-    <input placeholder="url to iiif manifest" type="url" @input="updateIiif" :value="defaultUrl"/>
+    <input
+      placeholder="url to iiif manifest"
+      type="url"
+      :value="defaultUrl"
+      @input="updateIiif" />
   </Container>
 </template>
 
